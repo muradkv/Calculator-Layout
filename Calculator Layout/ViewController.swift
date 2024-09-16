0970bd8cb065daf24dfb7a6557ba7b9ec9bff85f
murad <muradkv@gmail.com>
2024-09-16 12:50:29 +0300

    Initial commit

---
+++ Calculator Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Layout
+//
+//  Created by murad on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
